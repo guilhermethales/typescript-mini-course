057120b8a067af869700bfbdb673a6861d9e9289
Guilherme Thales <guilherme.thales@smarttbotter.com>
2020-06-22 00:18:06 -0300

    working with interfaces

---
+++ interfaces.ts
@@ -0,0 +1,53 @@
+interface Game {
+  id?: string | number;
+  title: string;
+  description: string;
+  readonly genre: string;
+  platform?: string[];
+  getSimilars?: (title: string) => void;
+}
+
+const tlou: Game = {
+  title: 'The last of Us',
+  description: 'The best game in the World',
+  genre: 'Action',
+  platform: ['PS3', 'PS4'],
+  getSimilars: (title: string) => {
+    console.log(`Similar games to ${title}: Uncharted, A Plague Tale, Metro`);
+  }
+}
+
+console.log(tlou.genre);
+
+if(tlou.getSimilars) {
+  tlou.getSimilars(tlou.title);
+}
+
+interface DLC extends Game {
+  originalGame: Game;
+  newContent: string[];
+}
+
+const leftbehind: DLC = {
+  title: 'The last of Us - Left Behind',
+  description: 'You play as Ellie before the original game',
+  genre: 'Action',
+  platform: ['PS4'],
+  originalGame: tlou,
+  newContent: ['3 hours story', 'new characters'],
+  getSimilars: (title: string) => {
+    console.log(`Similar games to ${title}: Uncharted, A Plague Tale, Metro`);
+  }
+}
+
+class CreateGame implements Game {
+  title: string;
+  description: string;
+  genre: string;
+
+  constructor(t: string, d: string, g:string) {
+    this.title = t;
+    this.description = d;
+    this.genre = g;
+  }
+}
